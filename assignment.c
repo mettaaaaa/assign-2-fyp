24d25dafbc059fb2b5b932e3e13550166442cf79
Metta <michellemettah@gmail.com>
2025-09-01 22:01:33 +0700

    Isi code diganti

--- assignment.c
+++ assignment.c
@@ -1,2 +1,10 @@
-'print("My name is Michelle Metta Hutama")' 
-'print("I like C")' 
+#include<stdio.h>
+
+int main(){
+	
+	printf("My name is Michelle Metta Hutama\n");
+	printf("I like C\n");
+	
+	return 0;
+	
+}
